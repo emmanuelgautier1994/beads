42ffa1eed7d09a8c3c845619e780036e42080d87
Emmanuel Gautier <emmanuel.gre@gmail.com>
2020-06-01 10:42:42 +0200

    Updates

---
+++ precache-manifest.eb2b0d31946c5a813af6d45d3dffceef.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eea66abc4360d3c2ff4e3d3ab4638c99",
+    "url": "/beads/index.html"
+  },
+  {
+    "revision": "4ffec2b89c65aa98f5eb",
+    "url": "/beads/static/css/main.e73bb9f4.chunk.css"
+  },
+  {
+    "revision": "83f5be2bd730006ac53a",
+    "url": "/beads/static/js/2.ca752955.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/beads/static/js/2.ca752955.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4ffec2b89c65aa98f5eb",
+    "url": "/beads/static/js/main.80332399.chunk.js"
+  },
+  {
+    "revision": "7eb059d6160310610cc0",
+    "url": "/beads/static/js/runtime-main.6aab70e8.js"
+  }
+]);
