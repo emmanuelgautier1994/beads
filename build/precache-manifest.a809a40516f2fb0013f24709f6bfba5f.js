ed26c8de81226979b4be06ee5a69c7b52ca6e2af
Emmanuel Gautier <emmanuel.gre@gmail.com>
2020-06-01 10:27:33 +0200

    build

---
+++ build/precache-manifest.a809a40516f2fb0013f24709f6bfba5f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8c17ab55d84b8e5d222081684840ef47",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b8925f7ae4f9fa580ad6",
+    "url": "/static/css/main.e73bb9f4.chunk.css"
+  },
+  {
+    "revision": "83f5be2bd730006ac53a",
+    "url": "/static/js/2.ca752955.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.ca752955.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b8925f7ae4f9fa580ad6",
+    "url": "/static/js/main.2ddca272.chunk.js"
+  },
+  {
+    "revision": "74b9ab21f9eb3ff607e9",
+    "url": "/static/js/runtime-main.a89599db.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
